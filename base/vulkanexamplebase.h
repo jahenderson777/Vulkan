6bb4b41d46b078ddc11c71c3d082ff9092e0016c
jahenderson777 <jahenderson777@gmail.com>
2020-10-27 09:00:36 +0000

    unfolding manifold

--- base/vulkanexamplebase.h
+++ base/vulkanexamplebase.h
@@ -157,8 +157,8 @@ public:
     void initSwapchain();
 	bool prepared = false;
 	bool resized = false;
-	uint32_t width = 1280;
-	uint32_t height = 720;
+	uint32_t width = 2560;
+	uint32_t height = 1440;
 
 	vks::UIOverlay UIOverlay;
 
