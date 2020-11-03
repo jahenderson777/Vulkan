11db2e22946457ef8bf3ef79bfa186b2f8286d94
jahenderson777 <jahenderson777@gmail.com>
2020-11-03 21:28:30 +0000

    election?

--- examples/computenbody/computenbody.cpp
+++ examples/computenbody/computenbody.cpp
@@ -15,7 +15,7 @@
 // Lower particle count on Android for performance reasons
 #define PARTICLES_PER_ATTRACTOR 3 * 1024
 #else
-#define PARTICLES_PER_ATTRACTOR 4 * 1024
+#define PARTICLES_PER_ATTRACTOR 24 * 1024
 #endif
 
 class VulkanExample : public VulkanExampleBase
